0333ff0e30e7b7ae9a806951aa5825571ca69b4e
f.coccia <f.coccia@legsolution.com>
2022-06-18 17:15:42 +0200

    1 - Add gem devise in Gemfile 2 - Add model User 3 - Add route devise in routes.rb 4 - Create a user in seeds.rb 5 - Add view users/sessions 6 - Add controller users/sessions

--- Gemfile
+++ Gemfile
@@ -19,7 +19,7 @@ gem "puma", "~> 5.0"
 gem "importmap-rails"
 
 # Hotwire's SPA-like page accelerator [https://turbo.hotwired.dev]
-gem "turbo-rails"
+# gem "turbo-rails"
 
 # Hotwire's modest JavaScript framework [https://stimulus.hotwired.dev]
 gem "stimulus-rails"
@@ -70,3 +70,5 @@ group :test do
   gem "selenium-webdriver"
   gem "webdrivers"
 end
+
+gem 'devise'
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,18 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  devise_for :users, controllers: {
+    sessions: 'users/sessions'
+  }
+
+  devise_scope :user do
+    authenticated :user do
+      root 'backend#index', as: :authenticated_root
+    end
+
+    unauthenticated do
+      root 'users/sessions#new', as: :unauthenticated_root
+    end
+  end
+
 end
