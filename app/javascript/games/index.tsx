2fbc30322b701ee8160312161b870dd308bae28c
f.coccia <f.coccia@legsolution.com>
2022-06-25 12:21:42 +0200

    1 - Add generation in file config.yaml 2 - Add app/javascript/games 3 - Add model Game

--- app/javascript/games/index.tsx
+++ app/javascript/games/index.tsx
@@ -1,5 +1,131 @@
-import React, {useEffect, useState} from 'react';
-import ReactDOM from 'react-dom';
+import React, { useState, useCallback, useRef } from "react";
+import ReactDOM from "react-dom";
+import produce from "immer";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<h1>Hello, worldd!</h1>);
+const numRows = 4;
+const numCols = 8;
+
+const operations = [
+  [0, 1],
+  [0, -1],
+  [1, -1],
+  [-1, 1],
+  [1, 1],
+  [-1, -1],
+  [1, 0],
+  [-1, 0]
+];
+
+const generateEmptyGrid = () => {
+  const rows = [];
+  for (let i = 0; i < numRows; i++) {
+    rows.push(Array.from(Array(numCols), () => 0));
+  }
+
+  return rows;
+};
+
+function Game () {
+  const [grid, setGrid] = useState(() => {
+    return generateEmptyGrid();
+  });
+
+  const [generation, setGeneration] = useState<Number>(0);
+
+  const [running, setRunning] = useState<boolean>(false);
+
+  const runningRef = useRef(running);
+  runningRef.current = running;
+
+  const runSimulation = useCallback(() => {
+    if (!runningRef.current) {
+      return;
+    }
+
+    setGrid(g => {
+      return produce(g, gridCopy => {
+        for (let i = 0; i < numRows; i++) {
+          for (let k = 0; k < numCols; k++) {
+            let neighbors = 0;
+            operations.forEach(([x, y]) => {
+              const newI = i + x;
+              const newK = k + y;
+              if (newI >= 0 && newI < numRows && newK >= 0 && newK < numCols) {
+                neighbors += g[newI][newK];
+              }
+            });
+
+            if (neighbors < 2 || neighbors > 3) {
+              gridCopy[i][k] = 0;
+            } else if (g[i][k] === 0 && neighbors === 3) {
+              gridCopy[i][k] = 1;
+            }
+          }
+          setGeneration(generation + 1);
+        }
+      });
+    });
+    setTimeout(runSimulation, 600);
+  }, []);
+
+  return (
+    <>
+      <button
+        onClick={() => {
+          setRunning(!running);
+          if (!running) {
+            runningRef.current = true;
+            runSimulation();
+          }
+        }}
+      >
+        {running ? "stop" : "start"}
+      </button>
+
+      <button
+        onClick={() => {
+          setGrid(generateEmptyGrid());
+        }}
+      >
+        clear
+      </button>
+
+      <div
+        style={{
+          display: "grid",
+          gridTemplateColumns: `repeat(${numCols}, 20px)`
+        }}
+      >
+        {grid.map((rows, i) =>
+          rows.map((col, k) => (
+            <div
+              key={`${i}-${k}`}
+              onClick={() => {
+                const newGrid = produce(grid, gridCopy => {
+                  gridCopy[i][k] = grid[i][k] ? 0 : 1;
+                });
+                setGrid(newGrid);
+              }}
+              style={{
+                width: 20,
+                height: 20,
+                backgroundColor: grid[i][k] ? "green" : undefined,
+                border: "solid 1px black"
+              }}
+            />
+          ))
+        )}
+        <div>
+          Generazione {generation}
+        </div>
+      </div>
+    </>
+  );
+};
+
+const rootElement = document.getElementById('gameOfLife');
+
+ReactDOM.render(
+  <Game />,
+  rootElement
+);
--- app/models/game.rb
+++ app/models/game.rb
@@ -1,28 +1,25 @@
 # Ruby implementation of Conway's Game of Life
-# run the program as : ruby game_of_life.rb 
+# run the program as : ruby game.rb
 
 require 'rbconfig'
 require 'yaml'
 
-
 class Game
 
-  # def self.windows?
-  #   !!(RbConfig::CONFIG['host_os'] =~ /mswin|mingw|cygwin/)
-  # end
-   
   attr_reader :matrix
+  @conf = YAML.load(File.read("config.yaml"))
 
   def initialize(rows, cols, seed)
-    @matrix = rows.times.map do |y| 
-      cols.times.map do |x| 
+    @matrix = rows.times.map do |y|
+      cols.times.map do |x|
         Cell.new(self, x, y, rand(seed).to_i.even?)
       end
     end
   end
-  
+
   def play
-    generation = 1
+    conf = YAML.load(File.read("config.yaml"))
+    generation = conf['generation']
     while(true)
       clear
       draw(generation)
@@ -31,11 +28,11 @@ class Game
       generation +=1
     end
   end
-  
+
   def self.play(rows, cols, seed=Time.now.to_i)
     Game.new(rows, cols, seed).play
   end
-  
+
   def clear
     system('cls') ? window? : system('clear')
   end
@@ -43,7 +40,7 @@ class Game
   def spinners
     @spinners ||= %w( | / -- \\ )
   end
-  
+
 
   def draw(generation)
     margin = "\t"
@@ -55,7 +52,7 @@ class Game
       matrix.map{|row| "= " + row.join(" ") + " =" },
       hline,
       "%2s Generation: #{generation}" % spinner
-    ].flatten.each do |line| 
+    ].flatten.each do |line|
       puts margin + line
     end
   end
@@ -67,7 +64,7 @@ class Game
       end
     end
   end
-  
+
   def cell_at(x, y)
     matrix.fetch(y){[]}[x]
   end
@@ -76,22 +73,22 @@ end
 class Cell
 
   attr_reader :x, :y, :game, :living, :neighbours
-  
+
   def initialize(game, x, y, state)
     @game = game
     @x = x
     @y = y
     @living = state
   end
-  
+
   def die
     @living = false
   end
-  
+
   def reproduce
     @living = true
   end
-  
+
   def alive?
     @living == true
   end
@@ -99,7 +96,7 @@ class Cell
   def dead?
     !alive?
   end
-  
+
   def neighbours
     @neighbours ||= begin
       [
@@ -114,21 +111,21 @@ class Cell
       ].compact
     end
   end
-  
+
   def live_neighbours
     neighbours.select(&:alive?)
   end
-  
+
   def under_populated?
     alive? and live_neighbours.size < 2
-  end  
-  
+  end
+
   def over_populated?
-    alive? and live_neighbours.size > 3  
-  end  
-  
+    alive? and live_neighbours.size > 3
+  end
+
   def can_be_reproduced?
-    dead? and live_neighbours.size == 3  
+    dead? and live_neighbours.size == 3
   end
 
   # Rules for next life state(copied from wiki):
@@ -137,15 +134,15 @@ class Cell
   #   - Any live cell with more than three live neighbours dies, as if by overcrowding.
   #   - Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
   def next_life_state
-    if under_populated? 
+    if under_populated?
       die
     elsif over_populated?
       die
     elsif can_be_reproduced?
       reproduce
-    end  
+    end
   end
-  
+
   def to_s
     alive? ? "*" : "."
   end
